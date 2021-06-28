d09ace42aa21398495b4ce4b82e10dd303f2b6da
minjujung <love053011@gmail.com>
2021-06-28 09:17:48 +0900

    Start Personal Project

---
+++ src/Create.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Create = (props) => {
+  return (
+    <>
+      <h1>Create</h1>
+    </>
+  );
+};
+
+export default Create;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = (props) => {
+  return (
+    <>
+      <h1>main</h1>
+    </>
+  );
+};
+
+export default Main;
